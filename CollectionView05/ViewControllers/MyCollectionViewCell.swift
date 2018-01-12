5fea84f56d19e61de49a69cc4901f075c186da55
Tifozoso <mfranks@gmail.com>
2018-01-11 22:22:15 -0800

    Initial Commit

---
+++ CollectionView05/ViewControllers/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionView05
+//
+//  Created by Michael Franks on 1/11/18.
+//  Copyright © 2018 Michael Franks. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+}
---
+++ CollectionView05/ViewControllers/MyCollectionViewController.swift
@@ -0,0 +1,89 @@
+//
+//  MyCollectionViewController.swift
+//  CollectionView05
+//
+//  Created by Michael Franks on 1/11/18.
+//  Copyright © 2018 Michael Franks. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "MyCell"
+
+class MyCollectionViewController: UICollectionViewController {
+
+    let cellName : [String] = ["cell 1", "cell 2", "cell 3", "cell 4", "cell 5", //"cell 6", "cell 7", "cell 8", "cell 9"//
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return cellName.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MyCollectionViewCell
+    
+        // Configure the cell
+    
+        cell.cellLabel.text = cellName[indexPath.row]
+        
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
